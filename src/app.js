93a79e377618a1c555ea9aaa3c3215a621343ad6
Matthew Willard <MWillard85@gmail.com>
2019-07-29 16:32:59 -0600

    fixed errors

--- src/app.js
+++ src/app.js
@@ -18,11 +18,13 @@ app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 app.use(validateBearerToken)
+
 app.use(bookmarksRouter)
 
 app.get('/', (req, res) => {
     res.send('Hello, world!')
   })
+  
 
 app.use(errorHandler)
 
